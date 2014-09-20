84defaf4623b2bb89a31af679b6cf29e137e1799
Gelin Su <skeletoncs@gmail.com>
2014-09-20 12:01:29 +0800

    init commit with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name openRecruitApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the openRecruitApp
+ */
+angular.module('openRecruitApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name openRecruitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the openRecruitApp
+ */
+angular.module('openRecruitApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
